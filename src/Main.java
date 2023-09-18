e095683b983c25a45f3fc07ca62fd0e40b23c11f
burgokmen <burgokmen@gmail.com>
2023-09-18 15:37:45 +0300

    hasTeen method is done

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,54 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        shouldWakeUp (true, 1);
 
-        shouldWakeUp(true, 7);
-        shouldWakeUp(false, 7);
-        shouldWakeUp(true, 12);
+        shouldWakeUp (false, 2);
 
-        shouldWakeUp(false, 12);
+        shouldWakeUp (true, 8);
+
+        shouldWakeUp (true, -1);
+
+        System.out.println("---------------------");
+
+        hasTeen(9, 99, 19);
+
+        hasTeen(23, 15, 42);
+
+        hasTeen(22, 23, 34);
     }
 
     public static boolean shouldWakeUp(boolean args, int num1){
         boolean ret = false;
 
         if(num1 < 0 || num1 > 23 ){
-            System.out.println("Boyle bi Saat yok" + ret);
+            System.out.println("Boyle bi Saat yok");
 
 
         } else if(num1 < 8 || num1 > 19 ) {
 
             if(args){
                 ret = true;
-                System.out.println("Uyandin" + ret);
+                System.out.println("Uyandin");
             }else{
-                ret = false;
-                System.out.println("Uyanmadin" + ret);
+
+                System.out.println("Uyanmadin");
 
             }
 
         } else {
-            System.out.println("Uyumuyosunki" + ret);
+            System.out.println("Uyumuyosunki");
 
         }
+        System.out.println(ret);
         return ret;
     }
+
+    public static boolean hasTeen(int num1, int num2, int num3){
+        boolean ret = ((19 >= num1) && (num1 >= 13)) || ((19 >= num2) && (num2 >= 13)) || ((19 >= num3) && (num3 >= 13));
+        System.out.println(ret);
+        return ret;
+    }
+
+
 }
